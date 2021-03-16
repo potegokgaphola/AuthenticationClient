529e0185c92d43b9225b4b465ba46ab923cd773e
Potego Kgaphola <seshupu@bbd.co.za>
2021-03-16 14:23:09 +0200

    fixed view engine error

--- app.js
+++ app.js
@@ -8,18 +8,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', './src/views');
 
-const formOptions = [
-  {
-    label: 'Email',
-    id: 'username'
-  },
-  {
-    label: 'Password',
-    id: 'password'
-  }
-];
 app.get('/', (req, res) => {
-  res.render('index', { formOptions });
+  res.render('index');
 });
 
 app.listen('8081', () => {
